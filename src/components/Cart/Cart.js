1a8ccb4cb23e3b4596154b8c9505debaa5c066fa
rahidhaque <ontirahid@yahoo.com>
2022-03-24 14:18:18 +0600

    Load Products

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,10 +1,25 @@
 import React from 'react';
-
+import './Cart.css'
 const Cart = (props) => {
+    const { cart } = props;
+    let total = 0;
+    let shipping = 0;
+    let quantity = 0;
+    for (const product of cart) {
+        quantity = quantity + product.quantity;
+        total = total + product.price * product.quantity;
+        shipping = shipping + product.shipping;
+    }
+    const tax = (total * 0.1).toFixed(2);
+    const grandTotal = total + shipping + parseFloat(tax);
     return (
-        <div>
+        <div className='cart'>
             <h3>Order Summary</h3>
-            <p>Selected Items: {props.cart.length}</p>
+            <p>Selected Items: {quantity}</p>
+            <p>Total Price: ${total}</p>
+            <p>Total Shipping Charge: ${shipping}</p>
+            <p>Tax: ${tax}</p>
+            <h5>Grand Total:${grandTotal.toFixed(2)} </h5>
         </div>
     );
 };
